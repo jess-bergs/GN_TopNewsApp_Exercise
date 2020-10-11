684e07edf22ca71cf9bdfccdcf92e193b0a0e19d
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-12 00:14:00 +0100

    Add newsApiFetcher with basic tests

---
+++ src/fetchers/fetchNewsApiHeadlinesForCountry.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import PropTypes from 'prop-types';
+
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
+
+const fetchNewsApiHeadlinesForCountry = async countryCode => {
+    const newsApiHeadlinesUrl = 'https://newsapi.org/v2/top-headlines';
+    const requestParams = { country: countryCode, apiKey: NEWS_API_KEY };
+    const headlinesRequestUrl = `${newsApiHeadlinesUrl}?${new URLSearchParams(requestParams)}`;
+    const error = errorMessage => {
+        throw new Error(
+            `Error when fetching headlines for country code ${countryCode}: ${errorMessage}`
+        );
+    };
+    let result;
+    try {
+        const req = await axios.get(headlinesRequestUrl);
+        result = req.data;
+    } catch (err) {
+        error(err.message);
+    }
+
+    return result.articles;
+};
+
+fetchNewsApiHeadlinesForCountry.propTypes = {
+    countryCode: PropTypes.string.isRequired,
+};
+
+export default fetchNewsApiHeadlinesForCountry;
---
+++ test/fetchers/fetchNewsApiHeadlinesForCountry.test.js
@@ -0,0 +1,35 @@
+import mockAxios from 'axios';
+import fetchNewsApiHeadlinesForCountry from '../../src/fetchers/fetchNewsApiHeadlinesForCountry';
+
+describe('The fetcher', () => {
+    const mockCountryCode = 'zz';
+
+    const getExpectedErrorMessage = (countryCode, errorMessage) =>
+        `Error when fetching headlines for country code ${countryCode}: ${errorMessage}`;
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('for a given country code, the fetcher returns the received articles', async () => {
+        const mockArticles = ['some article1', 'some article2'];
+
+        mockAxios.get.mockImplementationOnce(() =>
+            Promise.resolve({ data: { articles: mockArticles } })
+        );
+
+        const result = await fetchNewsApiHeadlinesForCountry(mockCountryCode);
+        expect(result).toEqual(mockArticles);
+    });
+
+    describe('if a network error occurs', () => {
+        it('it throws an error ', async () => {
+            const expectedError = 'some network error';
+            jest.mock('axios');
+            mockAxios.get.mockImplementationOnce(() => Promise.reject(new Error(expectedError)));
+            await expect(fetchNewsApiHeadlinesForCountry(mockCountryCode)).rejects.toThrow(
+                getExpectedErrorMessage(mockCountryCode, expectedError)
+            );
+        });
+    });
+});
