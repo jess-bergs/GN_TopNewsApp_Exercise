1c4bc3a6e72ab7173c8e4110fe80ed5a9f35ecfe
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-11 22:06:00 +0100

    AddPromo and PromoGrid to TopNewsPage (with hardcoded headlines)

--- src/pages/TopNewsPage.js
+++ src/pages/TopNewsPage.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import List from '../components/List';
 import ListItem from '../components/ListItem';
+import PromoGrid from '../components/PromoGrid';
+import Promo from '../components/Promo';
+import headlineFixtures from '../../test/fixtures/headlinesFixture.json';
 
 const TopNewsPage = ({ countries }) => (
     <div className="top-news-page__container">
@@ -10,6 +13,11 @@ const TopNewsPage = ({ countries }) => (
                 <ListItem title={country.name} key={country.countryCode} />
             ))}
         </List>
+        <PromoGrid>
+            {headlineFixtures.map((headline, index) => (
+                <Promo title={headline} key={index} />
+            ))}
+        </PromoGrid>
     </div>
 );
 
