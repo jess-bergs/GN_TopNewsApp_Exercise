d641923381f16267eef837dae30c824d1252cea0
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-11 21:26:49 +0100

    Add test for Promo to render title prop as text

--- test/components/Promo.test.js
+++ test/components/Promo.test.js
@@ -4,11 +4,16 @@ import Promo from '../../src/components/Promo';
 
 describe('The promo component', () => {
     let wrapper;
+    const mockTitle = 'mockTitle';
     beforeAll(() => {
-        wrapper = shallow(<Promo />);
+        wrapper = shallow(<Promo title={mockTitle} />);
     });
 
     it('should render with the correct base class', async () => {
         expect(wrapper.find('.promo__container')).toHaveLength(1);
+    });
+
+    it('should render the title props as text', async () => {
+        expect(wrapper.find('.promo__container').text()).toEqual(mockTitle);
     });
 });
