1f8a19c37d53f3a44cf15c7fe883279913bd6ac8
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-11 19:07:07 +0100

    Implement ListItem title prop passing & rendering

--- src/ListItem.js
+++ src/ListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const ListItem = () => <div className="list-item__container"></div>;
+const ListItem = ({ title }) => <div className="list-item__container">{title}</div>;
 
 export default ListItem;
