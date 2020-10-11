12453eecadd411a5f8902380b533d2beff263346
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-11 21:47:46 +0100

    Add test for PromoGrid to wrap its children

--- test/components/PromoGrid.test.js
+++ test/components/PromoGrid.test.js
@@ -5,10 +5,24 @@ import PromoGrid from '../../src/components/PromoGrid';
 describe('The promo grid component', () => {
     let wrapper;
     beforeAll(() => {
-        wrapper = shallow(<PromoGrid />);
+        wrapper = shallow(
+            <PromoGrid>
+                <div className="mockChild" />
+                <div className="mockChild" />
+                <div className="mockChild" />
+            </PromoGrid>
+        );
     });
 
     it('should render with the correct base class', async () => {
         expect(wrapper.find('.promo-grid__container')).toHaveLength(1);
+    });
+
+    it('should wrap its children', async () => {
+        expect(wrapper.find('.promo-grid__item-wrapper')).toHaveLength(3);
+        const itemWrappers = wrapper.find('.promo-grid__item-wrapper');
+        itemWrappers.forEach(itemWrapper => {
+            itemWrapper.find('.mockChild');
+        });
     });
 });
