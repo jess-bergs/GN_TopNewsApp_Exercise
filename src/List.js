52eec401f79dec13ffe1094833415d29984b3509
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-11 19:21:10 +0100

    Add test for ListItem to wrap children with li tag

--- src/List.js
+++ src/List.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const List = () => <div className="list__container" />;
+const List = ({ children }) => (
+    <ul className="list__container">
+        {children.map((child, index) => (
+            <li key={index}>{child}</li>
+        ))}
+    </ul>
+);
 
 export default List;
--- test/List.test.js
+++ test/List.test.js
@@ -5,10 +5,22 @@ import List from '../src/List';
 describe('The list component', () => {
     let wrapper;
     beforeAll(() => {
-        wrapper = shallow(<List />);
+        wrapper = shallow(
+            <List>
+                <div className="mockChild" />
+                <div className="mockChild" />
+                <div className="mockChild" />
+            </List>
+        );
     });
 
     it('should render with the correct base class', async () => {
         expect(wrapper.find('.list__container')).toHaveLength(1);
+    });
+
+    it('should render the children as a list', async () => {
+        console.log(wrapper.html());
+        expect(wrapper.find('ul')).toHaveLength(1);
+        expect(wrapper.find('li')).toHaveLength(3);
     });
 });
