689a99b8e4083926e7613914b9970f0e9f31f27e
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-11 19:06:12 +0100

    Add test for ListItem title prop rendering

--- test/ListItem.test.js
+++ test/ListItem.test.js
@@ -4,11 +4,16 @@ import ListItem from '../src/ListItem';
 
 describe('The list item', () => {
     let wrapper;
+    const mockTitle = 'mockTitle';
     beforeAll(() => {
-        wrapper = shallow(<ListItem />);
+        wrapper = shallow(<ListItem title={mockTitle} />);
     });
 
     it('should render with the correct base class', async () => {
         expect(wrapper.find('.list-item__container')).toHaveLength(1);
+    });
+
+    it('should render with the passes title prop', async () => {
+        expect(wrapper.find('.list-item__container').text()).toEqual(mockTitle);
     });
 });
