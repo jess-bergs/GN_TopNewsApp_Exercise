a97aeb09dca0a2d2f09c514c0cd1a1fe3068a6e2
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-11 21:49:15 +0100

    Refine test for list component

--- test/components/List.test.js
+++ test/components/List.test.js
@@ -21,5 +21,9 @@ describe('The list component', () => {
     it('should render the children as a list', async () => {
         expect(wrapper.find('ul')).toHaveLength(1);
         expect(wrapper.find('li')).toHaveLength(3);
+        const itemWrappers = wrapper.find('li');
+        itemWrappers.forEach(itemWrapper => {
+            itemWrapper.find('.mockChild');
+        });
     });
 });
