9e520bb1736a772f876df223830c789c8a9f4e7e
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-12 12:20:00 +0100

    Update ListWithDropdownOverflow to only render up to given count

--- src/components/ListWithDropdownOverflow.js
+++ src/components/ListWithDropdownOverflow.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import List from './List';
 
-const ListWithDropdownOverflow = ({ children }) => (
-    <div className="list-with-dropdown__container">
-        <div className="list-with-dropdown__list">
-            <List>{children}</List>
+const ListWithDropdownOverflow = ({ children, maxVisibleListItems }) => {
+    const getListItems = () => children.slice(0, maxVisibleListItems);
+
+    return (
+        <div className="list-with-dropdown__container">
+            <div className="list-with-dropdown__list">
+                <List>{getListItems()}</List>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default ListWithDropdownOverflow;
