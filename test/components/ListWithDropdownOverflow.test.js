50e315c39db183fa09c7f0df36d62f89a22ddd2b
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-12 12:18:16 +0100

    Add test for ListWithDropdownOverflow to render only up to given max count

--- test/components/ListWithDropdownOverflow.test.js
+++ test/components/ListWithDropdownOverflow.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import ListWithDropdownOverflow from '../../src/components/ListWithDropdownOverflow';
 
 describe('The list with dropdown-overflow component', () => {
     let wrapper;
+    const mockMaxVisibleListItems = 3;
     beforeAll(() => {
-        wrapper = shallow(
-            <ListWithDropdownOverflow>
+        wrapper = mount(
+            <ListWithDropdownOverflow maxVisibleListItems={mockMaxVisibleListItems}>
+                <div className="mockChild" />
+                <div className="mockChild" />
+                <div className="mockChild" />
                 <div className="mockChild" />
                 <div className="mockChild" />
                 <div className="mockChild" />
@@ -21,5 +25,12 @@ describe('The list with dropdown-overflow component', () => {
     it('should render a list with a List component', async () => {
         const listContainer = wrapper.find('.list-with-dropdown__list');
         expect(listContainer.find('List')).toHaveLength(1);
+    });
+
+    describe('for a given max count of visible Items', () => {
+        it('the list will only render the max count of ListItems', async () => {
+            const listContainer = wrapper.find('.list-with-dropdown__list').find('List');
+            expect(listContainer.find('.list__item-wrapper')).toHaveLength(mockMaxVisibleListItems);
+        });
     });
 });
