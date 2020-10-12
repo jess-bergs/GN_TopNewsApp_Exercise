594789f9c66b3e5e0da5940ccfe36d8baee8e0fd
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-12 12:05:39 +0100

    Add test for adding wrapped List comp to ListWithDropdownOverflow

--- test/components/ListWithDropdownOverflow.test.js
+++ test/components/ListWithDropdownOverflow.test.js
@@ -5,10 +5,21 @@ import ListWithDropdownOverflow from '../../src/components/ListWithDropdownOverf
 describe('The list with dropdown-overflow component', () => {
     let wrapper;
     beforeAll(() => {
-        wrapper = shallow(<ListWithDropdownOverflow />);
+        wrapper = shallow(
+            <ListWithDropdownOverflow>
+                <div className="mockChild" />
+                <div className="mockChild" />
+                <div className="mockChild" />
+            </ListWithDropdownOverflow>
+        );
     });
 
     it('should render with the correct base class', async () => {
         expect(wrapper.find('.list-with-dropdown__container')).toHaveLength(1);
+    });
+
+    it('should render a list with a List component', async () => {
+        const listContainer = wrapper.find('.list-with-dropdown__list');
+        expect(listContainer.find('List')).toHaveLength(1);
     });
 });
