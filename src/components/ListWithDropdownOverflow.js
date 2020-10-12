02bd82d19d2e8c87b67148fbb9fa351166425b64
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-12 13:31:15 +0100

    Adjust styling for List and listWithDropdownOverflow

--- src/components/ListWithDropdownOverflow.js
+++ src/components/ListWithDropdownOverflow.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import List from './List';
 import ListItem from './ListItem';
 
+import '../styles/ListWithDropdownOverflow.scss';
+
 const ListWithDropdownOverflow = ({ children, maxVisibleListItems }) => {
     const [showDropdown, setShowDropdown] = useState(false);
     const isOverflowing = () => children.length > maxVisibleListItems;
@@ -9,14 +11,14 @@ const ListWithDropdownOverflow = ({ children, maxVisibleListItems }) => {
     const toggleDropdown = () => (showDropdown ? setShowDropdown(false) : setShowDropdown(true));
 
     const DropDownToggleButton = () => (
-        <span className={'list-with-dropdown__toggle-button'}>
+        <span className={'list-with-dropdown__toggle-button'} key={children.length}>
             <ListItem title="more" onClick={() => toggleDropdown()} />
         </span>
     );
 
     const getListItems = () => {
         const items = children.slice(0, maxVisibleListItems);
-        if (isOverflowing()) items.push(<DropDownToggleButton key={children.length} />);
+        if (isOverflowing()) items.push(<DropDownToggleButton />);
         return items;
     };
 
