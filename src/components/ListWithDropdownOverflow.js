ef0baae189377310c566034851ec0222c4bd83c2
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-12 12:26:19 +0100

    Add more button for ListWithDropdownOverflow

--- src/components/ListWithDropdownOverflow.js
+++ src/components/ListWithDropdownOverflow.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import List from './List';
+import ListItem from './ListItem';
 
 const ListWithDropdownOverflow = ({ children, maxVisibleListItems }) => {
-    const getListItems = () => children.slice(0, maxVisibleListItems);
+    const isOverflowing = () => children.length > maxVisibleListItems;
+
+    const getListItems = () => {
+        const items = children.slice(0, maxVisibleListItems);
+        if (isOverflowing) items.push(<ListItem title="more" />);
+        return items;
+    };
 
     return (
         <div className="list-with-dropdown__container">
