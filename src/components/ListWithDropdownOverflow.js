dfb41cafd7af69a317f686ce7eb4f5944108761d
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-12 12:12:27 +0100

    Add List to ListWithDropdownOverflow

--- src/components/ListWithDropdownOverflow.js
+++ src/components/ListWithDropdownOverflow.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import List from './List';
 
-const ListWithDropdownOverflow = () => <div className="list-with-dropdown__container"></div>;
+const ListWithDropdownOverflow = ({ children }) => (
+    <div className="list-with-dropdown__container">
+        <div className="list-with-dropdown__list">
+            <List>{children}</List>
+        </div>
+    </div>
+);
 
 export default ListWithDropdownOverflow;
