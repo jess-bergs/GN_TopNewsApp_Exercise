577fc4593b32292f586833b1e1cb123f00d76ff2
Jess Bergs <jessica.bergs@bbc.co.uk>
2020-10-12 08:01:29 +0100

    Add basic styling to App container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import TopNewsPage from './pages/TopNewsPage';
 import topNewsPageConfig from './topNewsPageConfig.json';
 
+import './styles/App.scss';
+
 const App = () => (
     <div className="app__container">
         <TopNewsPage {...topNewsPageConfig} />
